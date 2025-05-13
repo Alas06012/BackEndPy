49acf9788a18dab9d5080ac8e0166210f96b7eae
lanserosk <ivan.osoriotobar@gmail.com>
2025-05-12 21:22:06 -0600

    StudyMaterial GCS
    Se agrego la capacidad de guardar un study material en la base y en un bucket de cloud storage
    
    llave de GCS se vende por separado

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class Config:
     MYSQL_DB = os.getenv('MYSQL_DB', 'english_test_app')
     MYSQL_CURSORCLASS = os.getenv('MYSQL_CURSORCLASS', 'DictCursor')
     DEEPSEEK_APIKEY = os.getenv('DEEPSEEK_APIKEY', '123456')
-    
+    GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME', 'tesisdev-bucket')
+    GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', 'storage-tesis.json')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'O8C6SqE5BpXmGVT1ePg0GML7GDK0HkQb')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
