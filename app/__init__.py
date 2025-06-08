c4a437c665357ccbfb4a3fb6cf1679847931ddd1
lanserosk <ivan.osoriotobar@gmail.com>
2025-06-07 18:02:56 -0600

    preparacion para merge

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,13 @@ from flask_mysqldb import MySQL
 from flask_jwt_extended import JWTManager
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_mail import Mail
 from config import Config
 from flask_cors import CORS
-
 # Inicializar extensiones
 mysql = MySQL()
 jwt = JWTManager()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
@@ -18,17 +19,8 @@ def create_app():
     # Inicializar extensiones
     mysql.init_app(app)
     jwt.init_app(app)
+    mail.init_app(app)
     
-    # limiter = Limiter(
-    #     key_func=get_remote_address,  # Usa la IP del usuario para limitar intentos
-    #     app=app,
-    #     default_limits=["5 per minute"]  # 5 intentos por minuto 
-    # )
-    
-    #CORS(app, resources={r"/register": {"origins": "http://localhost:5173"}})
-    #CORS(app)
-    #CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://host.docker.internal:5173", "http://host.docker.internal:5173/auth"]}})
-    # Modificar CORS para usar variables de entorno
     CORS(app, resources={
         r"/*": {
             "origins": os.environ.get('ALLOWED_ORIGINS').split(',')
--- config.py
+++ config.py
@@ -1,14 +1,34 @@
 import os
 from datetime import timedelta
 
+if os.environ.get("ENV") != "cloud":
+    from dotenv import load_dotenv
+    load_dotenv()
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'O8C6SqE5BpXmGVT1ePg0GML7GDK0HkQa')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'default_db')
-    MYSQL_CURSORCLASS = os.environ.get('MYSQL_CURSORCLASS', 'DictCursor')
+    # Configuración común
+    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'default_db')
+    MYSQL_CURSORCLASS = os.getenv('MYSQL_CURSORCLASS', 'DictCursor')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fallback_jwt_secret')
     
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'fallback_jwt_secret')
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    # Configuración específica de Cloud
+    DEEPSEEK_APIKEY = os.getenv('DEEPSEEK_APIKEY', '')
+    GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME', '')
+    GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', '')
+    
+    # Tokens JWT (ajustables por entorno)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=3)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    
+    # Configuración de email
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False') == 'False'
+    MAIL_DEFAULT_SENDER = (os.getenv('MAIL_SENDER_NAME', ''), os.getenv('MAIL_SENDER_EMAIL', ''))
